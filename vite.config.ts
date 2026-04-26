f9b358edaac8fab561bbbd98ea84e08286023462
eswasloc <eswasloc@gmail.com>
2026-04-27 01:20:30 +0900

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig(({ mode }) => ({
+  base: "/drink-log/",
   plugins: [react(), ...(mode === "https" ? [basicSsl()] : [])],
   server: {
     host: true,
